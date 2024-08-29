c255fd802d8aa44216d886cc9d84e88c79280100
marcel-bryan <marcelbryan0506@gmail.com>
2024-08-29 13:26:20 -0400

    Add files via upload

---
+++ 02_variables_and_data_types/Famous Quote 2.py
@@ -0,0 +1,4 @@
+quote='“Be the change that you wish to see in the world."'
+famous_person="- Mahatma Gandhi" #Fun fact Gandhi was loosely religious
+famous_quote=(f"{quote}\n{famous_person}")
+print(famous_quote)
