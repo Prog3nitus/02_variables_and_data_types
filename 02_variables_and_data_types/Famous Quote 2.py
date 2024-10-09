f0279573d3dedd73b84cf26aed3dc46e4c2b13b5
marcel-bryan <marcelbryan0506@gmail.com>
2024-10-09 09:12:58 -0400

    Delete 02_variables_and_data_types directory

--- 02_variables_and_data_types/Famous Quote 2.py
+++
@@ -1,4 +0,0 @@
-quote='“Be the change that you wish to see in the world."'
-famous_person="- Mahatma Gandhi" #Fun fact Gandhi was loosely religious
-famous_quote=(f"{quote}\n{famous_person}")
-print(famous_quote)
--- 02_variables_and_data_types/Simple Messages.py
+++
@@ -1,5 +0,0 @@
-message="coding is fun"
-print(message)
-#This is just for testing if the variable will change
-message="coding is a superpower"
-print(message)
